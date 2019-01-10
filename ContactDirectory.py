929dde7cbbbf17c69156e0bba547d4b99bbf612e
Nathan <natt888@yahoo.ca>
2019-01-10 15:19:07 -0700

    Made labels

--- ContactDirectory.py
+++ ContactDirectory.py
@@ -4,3 +4,22 @@ Nathan Tang
 19/01/09
 '''
 
+import tkinter as tk
+
+root = tk.Tk()
+root.wm_title("Contact Directory")
+
+title = tk.Label(root, 
+                font=('Tahoma', 12), 
+                text='Contact Directory')
+
+info = tk.Label(root, 
+                font=('Tahoma', 8), 
+                justify=tk.LEFT,
+                text="Store your contacts' names, emails, phone numbers, and addresses")
+
+title.pack()
+info.pack()
+
+
+root.mainloop()
